0dfddddab84a38567d4a40a4c4bd2975bd6efbbb
TaimurIshtiaq <56674122+TaimurIshtiaq@users.noreply.github.com>
2019-10-20 12:51:50 +0500

    Update rect.js

--- js/rect.js
+++ js/rect.js
@@ -1,37 +1,67 @@
-$(document).ready(function(){
-    let rows=5
-    let columns=5
-    createGrid();
-    // const width = $('#grid').width()/columns
-    // console.log('width', width)
-    // const height = $(document).height()/rows
-    // console.log('height', height)
-    // $('td').css('width',width)
-    // $('td').css('height',width)
+$(document).ready(function() {
+  let rows = 5;
+  let columns = 5;
+  createGrid();
+  numRectangles();
+  // const width = $('#grid').width()/columns
+  // console.log('width', width)
+  // const height = $(document).height()/rows
+  // console.log('height', height)
+  // $('td').css('width',width)
+  // $('td').css('height',width)
 
-    function createGrid(){
-        for (i = 0; i <= rows-1; i++) {
-            $('tbody').append('<tr id=' + i + '>')
-            for (j = 0; j <= columns-1; j++) {
-              //if($("td").hasClass())
-              $('#' + i).append('<td id=' + i + '_' + j + '></td>')
-            }
-            //$("tabody").append("</tr>");
+  function createGrid() {
+    for (i = 0; i <= rows - 1; i++) {
+      $("tbody").append("<tr id=" + i + ">");
+      for (j = 0; j <= columns - 1; j++) {
+        //if($("td").hasClass())
+        $("#" + i).append("<td id=" + i + "_" + j + "></td>");
+      }
+      //$("tabody").append("</tr>");
+    }
+  }
+
+  function numRectangles(arr = [[1, 1, 1], [1, 1, 1], [1, 0, 1]]) {
+    let counter = 0;
+    let pairs = [];
+    let rectangles = [];
+
+    console.log(arr);
+    for (var i = 0; i < arr.length - 1; i++) {
+      for (var j = i + 1; j < arr.length; j++) {
+        for (var k = 0; k < arr[i].length; k++) {
+          if (arr[i][k] == arr[j][k] && arr[j][k] == 1) {
+            temp = [[i, k], [j, k]];
+            pairs.push(temp);
           }
+        }
+      }
     }
 
-    $('tbody tr td').click(function(event){
-        let pointId=event.target.id
-        let point=$('#'+pointId)
-        let IsPoint=point.hasClass('deletedPoint')
-        
-        if(!IsPoint){
-            point.addClass('deletedPoint')
+    for (i = 0; i < pairs.length - 1; i++) {
+      for (j = i + 1; j < pairs.length; j++) {
+        if (
+          pairs[i][0][0] == pairs[j][0][0] &&
+          pairs[i][1][0] == pairs[j][1][0]
+        ) {
+          rectangles.push([pairs[i][0], pairs[j][0], pairs[i][1], pairs[j][1]]);
         }
-        else{
-            point.removeClass('deletedPoint')
-        }
-        
+      }
+    }
+    console.log(rectangles);
+    console.log(rectangles.length);
+    return rectangles;
+  }
 
-    })
-})
+  $("tbody tr td").click(function(event) {
+    let pointId = event.target.id;
+    let point = $("#" + pointId);
+    let IsPoint = point.hasClass("deletedPoint");
+
+    if (!IsPoint) {
+      point.addClass("deletedPoint");
+    } else {
+      point.removeClass("deletedPoint");
+    }
+  });
+});
