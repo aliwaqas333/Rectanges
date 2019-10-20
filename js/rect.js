b7a463fe93a9a54d11dfe38179ea669780208ea8
TaimurIshtiaq <56674122+TaimurIshtiaq@users.noreply.github.com>
2019-10-20 14:28:10 +0500

    Update rect.js

--- js/rect.js
+++ js/rect.js
@@ -37,7 +37,6 @@ $(document).ready(function() {
     let pairs = [];
     let rectangles = [];
 
-    console.log(arr);
     for (var i = 0; i < arr.length - 1; i++) {
       for (var j = i + 1; j < arr.length; j++) {
         for (var k = 0; k < arr[i].length; k++) {
